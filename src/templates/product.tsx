95ff87154ee142184cc34a5f1e84d4e1b7b5f007
Max Shaw <Maxdshaw@gmail.com>
2023-02-15 20:04:38 -0500

    Yext Studio Commit

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -12,7 +12,6 @@ import ProductImage from "../components/ProductImage";
 import ProductTable from "../components/ProductTable";
 import Reviews from "../components/Reviews";
 import Section from "../components/Section";
-import SkiHomeLayout from "../components/SkiHomeLayout";
 import VStack from "../components/VStack";
 import "../index.css";
 
@@ -31,56 +30,54 @@ export const getPath: GetPath<TemplateProps> = ({ document }) => {
 export default function Product({ document }: TemplateProps) {
   return (
     <>
-      <SkiHomeLayout>
-        <GridContainer>
-          <VStack>
-            <Headline value={document.name} fontWeight="bold" textSize="4xl" />
-            <HStack>
-              <Label value={`$${document.c_price}`} />
-              <Reviews averageRating={5} reviewCount={1995} />
-            </HStack>
-          </VStack>
-          <ProductImage
-            src={`${document.photoGallery[0].image.url}`}
-            alt="Light green backpack with black canvas straps and front zipper pouch."
+      <GridContainer>
+        <VStack>
+          <Headline value={document.name} fontWeight="bold" textSize="4xl" />
+          <HStack>
+            <Label value={`$${document.c_price}`} />
+            <Reviews averageRating={5} reviewCount={1995} />
+          </HStack>
+        </VStack>
+        <ProductImage
+          src={`${document.photoGallery[0].image.url}`}
+          alt="Light green backpack with black canvas straps and front zipper pouch."
+        />
+      </GridContainer>
+      <Section background={false}>
+        <ProductTable
+          title="Product Details"
+          rockerType="All Mountain Rocker – Smooth rocker at both tip and tail with camber underfoot provides balanced performance anywhere on the mountain."
+          shape="True Tip LT – By trimming the amount of heavy ABS plastic in the tip and extending the lightweight wood core into this area with a taper that further reduces weight, Nordica enhances playfulness and maneuverability of the ski without compromising that unmatchable feeling of powerful and control."
+          core="Lite Performance Wood | Poplar / Beech"
+          laminates="Carbon Chassis LT – A proprietary blend of carbon materials to reduce weight while maximizing performance."
+          sidewalls="ABS Sidewalls"
+          bindingCompatibility="We recommend a brake width equal to or at most 15 mm wider than the ski waist width."
+        />
+      </Section>
+      <Section background={false}>
+        <ItemsGrid title="Similar Items">
+          <Item
+            name="Nordica Enforcer 100 Skis 2023"
+            image="https://images.evo.com/imgp/250/224769/902350/nordica-enforcer-100-skis-2023-.jpg"
+            price={799.99}
           />
-        </GridContainer>
-        <Section background={false}>
-          <ProductTable
-            title="Product Details"
-            rockerType="All Mountain Rocker – Smooth rocker at both tip and tail with camber underfoot provides balanced performance anywhere on the mountain."
-            shape="True Tip LT – By trimming the amount of heavy ABS plastic in the tip and extending the lightweight wood core into this area with a taper that further reduces weight, Nordica enhances playfulness and maneuverability of the ski without compromising that unmatchable feeling of powerful and control."
-            core="Lite Performance Wood | Poplar / Beech"
-            laminates="Carbon Chassis LT – A proprietary blend of carbon materials to reduce weight while maximizing performance."
-            sidewalls="ABS Sidewalls"
-            bindingCompatibility="We recommend a brake width equal to or at most 15 mm wider than the ski waist width."
+          <Item
+            name="Atomic Bent 100 Skis 2023"
+            image="https://images.evo.com/imgp/250/219400/896899/atomic-bent-100-skis-2023-.jpg"
+            price={599.95}
           />
-        </Section>
-        <Section background={false}>
-          <ItemsGrid title="Similar Items">
-            <Item
-              name="Nordica Enforcer 100 Skis 2023"
-              image="https://images.evo.com/imgp/250/224769/902350/nordica-enforcer-100-skis-2023-.jpg"
-              price={799.99}
-            />
-            <Item
-              name="Atomic Bent 100 Skis 2023"
-              image="https://images.evo.com/imgp/250/219400/896899/atomic-bent-100-skis-2023-.jpg"
-              price={599.95}
-            />
-            <Item
-              name="Saloman QST 98 Skis 2023"
-              image="https://images.evo.com/imgp/250/197273/812057/salomon-qst-98-skis-2023-.jpg"
-              price={599.95}
-            />
-            <Item
-              name="Nordica Enforcer 88 Skis"
-              image="https://images.evo.com/imgp/250/224771/910233/nordica-enforcer-88-skis-2023-.jpg"
-              price={699.99}
-            />
-          </ItemsGrid>
-        </Section>
-      </SkiHomeLayout>
+          <Item
+            name="Saloman QST 98 Skis 2023"
+            image="https://images.evo.com/imgp/250/197273/812057/salomon-qst-98-skis-2023-.jpg"
+            price={599.95}
+          />
+          <Item
+            name="Nordica Enforcer 88 Skis"
+            image="https://images.evo.com/imgp/250/224771/910233/nordica-enforcer-88-skis-2023-.jpg"
+            price={699.99}
+          />
+        </ItemsGrid>
+      </Section>
     </>
   );
 }
